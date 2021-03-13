083024d196aa4da61c2349f469594af2ad2b7fbb
noaione <noaione0809@gmail.com>
2021-03-13 23:42:07 +0700

    Export modul tesaurus dengan __init__

---
+++ tesaurus/__init__.py
@@ -0,0 +1,11 @@
+"""
+:mod:`tesaurus` -- Modul Tesaurus Tematis Python
+================================================
+
+.. module:: tesaurus
+    :platform: Unix, Windows, Mac
+    :synopsis: Modul ini mengandung implementasi scrapper Tesaurus Tematis Kemendikbud
+.. moduleauthor:: noaione <noaione0809@gmail.com>
+"""
+
+from .tesaurus import *  # NOQA
