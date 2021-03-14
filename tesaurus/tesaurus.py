204c17c95d4d414cf0d6149ff094b491e1ca828f
noaione <noaione0809@gmail.com>
2021-03-14 12:27:15 +0700

    Ubah sistem objek dan buat versi async Sekarang harus memanggil fungsi `cari` untuk mencari kata

--- tesaurus/tesaurus.py
+++ tesaurus/tesaurus.py
@@ -8,38 +8,64 @@
 .. moduleauthor:: noaione <noaione0809@gmail.com>
 """
 
+import asyncio
+from types import SimpleNamespace
+
+import aiohttp
 import requests
 from bs4 import BeautifulSoup
 
 
 class Tesaurus:
-    """Sebuah hasil lema di Tesaurus Tematis"""
+    """Sebuah instansi modul Tesaurus"""
 
     __HOST = "https://tesaurus.kemdikbud.go.id/tematis/lema"
 
-    def __init__(self, kueri: str, kelas_kata: str = None) -> None:
-        """Membuat objek Tesaurus baru berdasarkan kueri yang diberikan.
+    def __init__(self, sesi: requests.Session = None) -> None:
+        """Membuat objek Tesaurus baru
 
-        :param kueri: Kata kunci pencarian
-        :type kueri: str
-        :param loop: Sebuah event loop asyncio
+        :param sesi: sesi kustom untuk dipakai
+        :type sesi: reequest.Session
         """
 
-        self.nama = kueri
-        if isinstance(kelas_kata, str):
-            kelas_kata = kelas_kata.lower()
-        self.kelas_kata = kelas_kata
+        self.kata: str
+        self.kelas_kata: str
         self.hasil = []
         self.saran = []
 
-        self.sesi = requests.Session()
+        if isinstance(sesi, (requests.Session, aiohttp.ClientSession)):
+            self.sesi = sesi
+        else:
+            self.sesi = requests.Session()
+
+    def cari(self, kueri: str, kelas_kata: str = None):
+        """Mencari kata atau lema di Tesaurus
+
+        :param kueri: kata atau kalimat yang ingin dicari
+        :type kueri: str
+        :param kelas_kata: kelas kata, defaults to None
+        :type kelas_kata: str, optional
+        """
+        self._reset_data()
+        self.kata = kueri
+        if isinstance(kelas_kata, str):
+            kelas_kata = kelas_kata.lower()
+        self.kelas_kata = kelas_kata
         laman_url = self._buat_url()
         laman = self.sesi.get(laman_url)
         self._cek_galat(laman)
         self._buat_entri(laman.text)
 
+    def _reset_data(self):
+        """Jangan dipakai, ini merupakan fungsi internal yang akan dipanggil otomatis"""
+        self.hasil = []
+        self.saran = []
+        self.kata = None
+        self.kelas_kata = None
+
     def _buat_url(self):
-        base_url = f"{self.__HOST}/{self.nama}"
+        """Jangan dipakai, ini merupakan fungsi internal yang akan dipanggil otomatis"""
+        base_url = f"{self.__HOST}/{self.kata}"
         valid_kelas = ["adverbia", "konjungsi", "nomina", "numeralia", "partikel", "verba"]
         if isinstance(self.kelas_kata, str):
             if self.kelas_kata not in valid_kelas:
@@ -48,15 +74,19 @@ class Tesaurus:
         return base_url
 
     def _cek_galat(self, laman: requests.Response):
+        """Jangan dipakai, ini merupakan fungsi internal yang akan dipanggil otomatis"""
         if laman.status_code != 200:
+            if laman.status_code == 404:
+                raise TidakDitemukan(self.kata, self.kelas_kata)
             raise TerjadiKesalahan()
         not_found_text = "dari semua kelas kata tidak ditemukan"
         if isinstance(self.kelas_kata, str):
             not_found_text = f"dari kelas kata {self.kelas_kata} tidak ditemukan"
         if not_found_text in laman.text:
-            raise TidakDitemukan(self.nama, self.kelas_kata)
+            raise TidakDitemukan(self.kata, self.kelas_kata)
 
     def _buat_entri(self, laman: str):
+        """Jangan dipakai, ini merupakan fungsi internal yang akan dipanggil otomatis"""
         sup = BeautifulSoup(laman, "html.parser")
         htmlstr = ""
         result_contain = sup.find("div", {"class": "contain"})
@@ -68,6 +98,41 @@ class Tesaurus:
                 htmlstr = str(child)
             else:
                 htmlstr += str(child)
+
+
+class TesaurusAsync(Tesaurus):
+    """Sebuah instansi asynchronous untuk modul Tesaurus"""
+
+    def __init__(self, sesi: aiohttp.ClientSession = None, loop: asyncio.AbstractEventLoop = None) -> None:
+        """Membuat objek Tesaurus Baru untuk dipakai di fungsi async.
+
+        :param sesi: Sesi aiohttp kustom yang ingin digunakan, defaults to None
+        :type sesi: aiohttp.ClientSession, optional
+        """
+        super().__init__()
+        if not isinstance(loop, asyncio.AbstractEventLoop):
+            loop = asyncio.get_event_loop()
+        if isinstance(sesi, aiohttp.ClientSession):
+            self.sesi = sesi
+        else:
+            self.sesi = aiohttp.ClientSession(loop=loop)
+
+    async def cari(self, kueri: str, kelas_kata: str):
+        self._reset_data()
+        self.kata = kueri
+        if isinstance(kelas_kata, str):
+            kelas_kata = kelas_kata.lower()
+        self.kelas_kata = kelas_kata
+        laman_url = self._buat_url()
+        try:
+            req = await self.sesi.get(laman_url)
+        except (aiohttp.ClientConnectionError, aiohttp.ClientError, TimeoutError):
+            raise TerjadiKesalahan()
+        teks = await req.text()
+        # Buat namespace yang mirip seperti requests.get()
+        laman = SimpleNamespace(text=teks, status_code=req.status)
+        self._cek_galat(laman)
+        self._buat_entri(teks)
 
 
 class LemaEntri:
