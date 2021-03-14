6d9c7b5a7c1cf0393ad05c01736ec8acd3980d56
noaione <noaione0809@gmail.com>
2021-03-14 19:10:38 +0700

    tests: tambah tes untuk fitur terkait

--- tests/test_tesaurus.py
+++ tests/test_tesaurus.py
@@ -7,10 +7,12 @@ from tesaurus import TidakDitemukan
 _KASUS_PATH = os.path.join(os.getcwd(), "tests", "kasus")
 
 
-def read_kasus(nama, kelas=None):
+def read_kasus(nama, kelas=None, terkait=False):
     path = nama
     if kelas:
         path += f"_{kelas}"
+    if terkait:
+        path += "_terkait"
     path += ".txt"
     with open(os.path.join(_KASUS_PATH, path), "r", encoding="utf-8") as fp:
         return fp.read()
@@ -29,6 +31,14 @@ def test_dengan_kelas_kata():
     te = MockTesaurus()
     te.cari("makan", "adjektiva")
     res = str(te).rstrip("\n")
+    assert res == kasus
+
+
+def test_kata_dengan_terkait():
+    kasus = str(read_kasus("makan", None, True)).rstrip("\n")
+    te = MockTesaurus()
+    te.cari("makan")
+    res = te.__str__(terkait=True).rstrip("\n")
     assert res == kasus
 
 
